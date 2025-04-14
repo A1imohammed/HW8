60990027efd3c02f2f01ff4ac2410d19cd58de2f
A1imohammed <moalimohammed7@gmail.com>
2025-04-13 23:27:52 -0400

    Create Blink_derek_LED.h

---
+++ Blink_derek_LED.h
@@ -0,0 +1,23 @@
+#ifndef DEREK_LED_H
+#define DEREK_LED_H
+
+#include <string>
+using std::string;
+
+class LED {
+           private:
+                   string path;
+                   int number;
+                   virtual void writeLED(string filename, string value);
+                   virtual void removeTrigger();
+        public:
+              LED(int number);
+              virtual void turnOn();
+              virtual void turnOff();
+              virtual void flash(string delayms = "50");
+              virtual void blink(int num);
+              virtual void outputState();
+              virtual ~LED();
+};
+
+#endif
